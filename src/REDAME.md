19588d89da3e4c3df3fa346e33be6bbf5d8ce5d6
jeremy <Vous@exemple.com>
2025-04-09 11:33:18 +0200

    update script for data power interprétation

---
+++ src/REDAME.md
@@ -0,0 +1,12 @@
+
+
+
+git clone 
+
+cd src/
+
+source env_panda/bin/activate
+
+cd test/
+
+python3 main2.py
--- src/main.py
+++ src/main.py
@@ -6,27 +6,27 @@ import pandas as pd
         
 def main():
         # Consommation Moyenne
-        moyenne = Moyen_Consommation("/home/garcon/Documents/gitlab/data_and_ia/src/data/data_énergie_trié_modifié.csv", 'Conso totale (MWh)')
+        moyenne = Moyen_Consommation("/home/garcon/Documents/github/data_and_ia/src/data/eCO2mix_RTE_Bretagne_Annuel-Definitif_2022.csv", 'Conso totale (MWh)')
         print(f"\n - La consommation moyenne est de {moyenne} MWh sur la Commune de Rennes")
 
         # Consommation Totale
-        total = Total_Consommation("/home/garcon/Documents/gitlab/data_and_ia/src/data/data_énergie_trié_modifié.csv", 'Conso totale (MWh)')
+        total = Total_Consommation("/home/garcon/Documents/github/data_and_ia/src/data/eCO2mix_RTE_Bretagne_Annuel-Definitif_2022.csv", 'Conso totale (MWh)')
         print(f"\n - La consommation totale est de {total} MWh sur la Commune de Rennes")
 
         # Consommation par habitant
-        moyenne_par_habitant, total_par_habitant = Consommation_Par_Habitant("/home/garcon/Documents/gitlab/data_and_ia/src/data/data_énergie_trié_modifié.csv")
+        moyenne_par_habitant, total_par_habitant = Consommation_Par_Habitant("/home/garcon/Documents/github/data_and_ia/src/data/eCO2mix_RTE_Bretagne_Annuel-Definitif_2022.csv")
         print(f"\n - La consommation moyenne par habitant est de {moyenne_par_habitant} MWh")
         print(f"\n - La consommation totale par habitant est de {total_par_habitant} MWh")
         
         try:
-            meteo_display_temperature_on_data("/home/garcon/Documents/gitlab/data_and_ia/src/data/donne_meteorologique.csv", "2013-04-06")
+            meteo_display_temperature_on_data("/home/garcon/Documents/github/data_and_ia/src/data/donne_meteorologique.csv", "2013-04-06")
         except FileNotFoundError:
             print("Le fichier 'donne_meteorologique.csv' est introuvable. Veuillez vérifier le chemin ou l'emplacement du fichier.")
             
             
-        Graphique_Température_Annuelle("/home/garcon/Documents/gitlab/data_and_ia/src/data/donne_meteorologique.csv")
+        # Graphique_Température_Annuelle("/home/garcon/Documents/github/data_and_ia/src/data/donne_meteorologique.csv")
         
-        Graphique_Consommation_Mensuelle("/home/garcon/Documents/gitlab/data_and_ia/src/data/eCO2mix_RTE_tempo_2023-2024.csv")
+        Graphique_Consommation_Mensuelle("/home/garcon/Documents/github/data_and_ia/src/data/eCO2mix_RTE_Bretagne_Annuel-Definitif_2022.csv")
         
     # Exemple d'utilisation 
 main()
